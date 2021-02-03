86eff8065a6c3f96e79594d85898713c0bc2cc2c
veritaS <veritas@very-coding.de>
2021-02-03 20:43:42 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,22 @@
+// Disable the toolbar for authors only
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
+// Turn off logout confirmation and redirect logout to home
+add_action('check_admin_referer', 'logout_without_confirm', 10, 2);
+function logout_without_confirm($action, $result)
+{
+    /**
+     * Allow logout without confirmation
+     */
+    if ($action == "log-out" && !isset($_GET['_wpnonce'])) {
+        $redirect_to = isset($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : home_url() ;
+        $location = str_replace('&amp;', '&', wp_logout_url($redirect_to));
+        header("Location: $location");
+        die;
+    }
+}
